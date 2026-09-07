d7200306fbc54c63a444472492b02e41a1f36b8e
Greyson Lizanecz <glizanecz@gmail.com>
2026-09-06 23:54:23 -0400

    more changes idk...

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const outcomeOfTrains = document.getElementById("outcomeOfTrains");
 const humanInput = document.getElementById("humanInput");
 const robotOutput = document.getElementById("robotOutput");
 const submitQuestion = document.getElementById("submitQuestion");
+let layers = 4;
+let nodesPerLayer = [10, 15, 15, 367]
 let weights = [];
 let oldWeights = [];
 let biases = [];
@@ -12,7 +14,6 @@ let oldBiases = [];
 let wordObjects = [];
 let trainingSentences = [];
 let learningRate = .01;
-
 
 numOfRuns.oninput = function() {
     if (Number(numOfRuns.value) === 0) {
@@ -44,10 +45,61 @@ window.textToNumbers = function(input) {
 }
 
 async function trainOneEpoch(count) {
+    let testSentence = textToNumbers(trainingSentences[count].sentence);
+    let testTargets = Array(wordObjects.length, 0);
+    testTargets[textToNumbers(trainingSentences[count].correctWord)] = 1;
     let sums = [];
-    oldWeights = [...weights];
-    oldBiases = [...biases];
-    
+    let listBeforeOutcome = [];
+    let layerInp = [...testSentence];
+    let preActivatedSums = [];
+    oldWeights = structuredClone(weights);
+    oldBiases = structuredClone(biases);
+    weights = [];
+    biases = [];
+    for (let init = 0; init < layers; init++) {
+        weights.push([]);
+        biases.push([]);
+        preActivatedSums([]);
+        for (let init2 = 0; init2 < nodesPerLayer[init]; init2++) {
+            weights[init].push([]);
+            biases[init].push([]);
+            preActivatedSums[init].push([]);
+        }
+    }
+    for (let layer = 1; layer < layers; layer++) {
+        let totalSums = [];
+        for (let node = 0; node < nodesPerLayer[layer]; node++) {
+            let totalSum = 0;
+            for (let prevLayerOne = 0; prevLayerOne < layerInp.length; prevLayerOne++) {
+                if (count === 0) {
+                    let w = Math.random();
+                    weights[layer][node].push(w);
+                    totalSum += w * layerInp[prevLayerOne];
+                } else {
+                    let w = oldWeights[layer][node][prevLayerOne];
+                    weights[layer][node].push(w);
+                    totalSum += w * layerInp[prevLayerOne];
+                }
+            }        
+            if (count === 0) {
+                let b = Math.random();
+                biases[layer][node] = b;
+                totalSum += b;
+                let outputForRelu = relu(totalSum)
+                totalSums.push(outputForRelu)
+            } else {
+                let b = oldBiases[layer][node];
+                biases[layer][node] = b;
+                totalSum += b;
+                let outputForRelu = relu(totalSum)
+                totalSums.push(outputForRelu)
+            }
+            preActivatedSums[layer][node] = totalSum;
+        }
+        layerInp = [];
+        layerInp.push(...totalSums)
+    };
+    calcNewWgtsNBias(weights, biases, testSentence, testTargets, layerInp, preActivatedSums);
 }
 
 const randomWeights = Array.from({ length: getWordObjects().length }, () => Math.random());
@@ -55,7 +107,24 @@ const randomBiases = Array.from({ length: getWordObjects().length }, () => Math.
 weights = [...randomWeights];
 biases = [...randomBiases];
 
+function relu(x) {
+    return Math.max(0, x)
+}
 
+function calcNewWgtsNBias(weightList, biasList, sentence, targets, listBeforeOutcome, preActivatedSums) {
+    let highestOutcome = 0;
+    let highestIndex = 0;
+    let errors = [];
+    for (let i = 0; i < listBeforeOutcome.length; i++) {
+        if (listBeforeOutcome[i] > highestOutcome) {
+            highestOutcome = listBeforeOutcome[i];
+            highestIndex = i;
+        }
+        let error = targets[i] - listBeforeOutcome[i];
+        errors.push(error);
+    }
+
+}
 
 function getWordObjects() {
     for (let i = 0; i < phrases.length; i++) {
@@ -96,7 +165,7 @@ function getTrainingSentences() {
     }
 }
 
-console.log(getTrainingSentences())
+getTrainingSentences()
 
 function testFunc() {
     let text = "How was your day today?"
